d1b0f9c3f973e0987baff6d7efc1ad7241c90f0a
Drew Conly <atconly@gmail.com>
2016-07-04 19:33:07 -0600

    get prompts working and arrays updating

---
+++ main.js
@@ -0,0 +1,25 @@
+var victimsInput = prompt("How many victims?")
+var vicNames = [];
+var vicNumbers = [];
+var vicAddresses = [];
+
+for (i = 0; i < parseInt(victimsInput); i++) {
+  vicNames.push(prompt("Victim's Name?"));
+  vicNumbers.push(prompt("Victim's Phone Number?"));
+  vicAddresses.push(prompt("Victim's Address?"));
+}
+
+var volunteersInput = prompt("How many volunteers?")
+var volNames = [];
+var volNumbers = [];
+var volAddresses = [];
+
+for (i = 0; i < parseInt(victimsInput); i++) {
+  volNames.push(prompt("Volunteer's Name?"));
+  volNumbers.push(prompt("Volunteer's Phone Number?"));
+  volAddresses.push(prompt("Volunteers's Address?"));
+}
+
+function vicVol() {
+  
+}
